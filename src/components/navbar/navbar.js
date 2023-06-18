e5de550beb3c4ac287d80ead6d7cde0fb8a092a3
Rovind R <112811209+Rovind16@users.noreply.github.com>
2023-06-18 14:35:25 +0530

    Update navbar.js

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -15,7 +15,7 @@ const Navbar = () => {
     localStorage.clear();
     window.localStorage.setItem('loggedIn', false);
     localStorage.setItem('TotalAmount', 0);
-    window.location.href = "https://bookie-gekz.onrender.com/home";
+   window.location.href = "./home";
     window.alert('Logout Successful');
   }
 
