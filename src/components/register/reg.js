d2faf946f5fe379d354fbd241ae139dae4a49116
Rovind R <112811209+Rovind16@users.noreply.github.com>
2023-06-22 20:39:31 +0530

    Update reg.js

--- src/components/register/reg.js
+++ src/components/register/reg.js
@@ -1,6 +1,6 @@
-import"./reg.css";
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { Link } from "react-router-dom";
+
 export default class App extends Component {
   
   constructor(props) {
@@ -9,105 +9,115 @@ export default class App extends Component {
       fname: "",
       lname: "",
       email: "",
-      password: "",
+      password: ""
     };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
+
   handleSubmit(e) {
-    e.preventDefault();   
+    e.preventDefault();
     const { fname, lname, email, password } = this.state;
-    var value=[];
+    var value = [];
     value.push(fname);
     value.push(lname);
     value.push(email);
     value.push(password);
-    var f=0;
-    for(var i=0;i<value.length;i++){
-      if(value[i]==0){
-        f=1;
-        alert('All field should be filled');
+    var f = 0;
+    for (var i = 0; i < value.length; i++) {
+      if (value[i] === "") {
+        f = 1;
+        alert("All fields should be filled");
         break;
       }
     }
     var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    if (!(email.match(validRegex))) {
-      f=1;
-      alert('Email is invalid')
+    if (!email.match(validRegex)) {
+      f = 1;
+      alert("Email is invalid");
     }
     console.log(fname, lname, email, password);
-    if(f==0){
-     
-    fetch("https://bookie-backend.onrender.com/register", {
-      method: "POST",
-      crossDomain: true,
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-        "Access-Control-Allow-Origin": "*",
-      },
-      body: JSON.stringify({
-        fname,
-        email,
-        lname,
-        password,
-      }),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data, "userRegister");
-      });
-      alert('sign up successfull')
-      window.location.href = '/login'
+    if (f === 0) {
+      fetch("https://bookie-backend.onrender.com/register", {
+        method: "POST",
+        crossDomain: true,
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          "Access-Control-Allow-Origin": "*"
+        },
+        body: JSON.stringify({
+          fname,
+          email,
+          lname,
+          password
+        })
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data, "userRegister");
+        });
+      alert("Sign up successful");
+      window.location.href = "/login";
+    }
   }
-}
+
   render() {
     return (
       <body>
-      <div className='box'>
-       <form onSubmit={this.handleSubmit}>
-        <h3>Sign Up</h3>
-          <h5><label>First name:</label></h5>
-          <input
-            type="text"
-            className="user"
-            placeholder="First name" 
-            onChange={(e) => this.setState({ fname: e.target.value })}
-            />
-          <h5><label>Last name:</label></h5>
-          <input
-            type="text"
-            className="user"
-            placeholder="Last name" 
-            onChange={(e) => this.setState({ lname: e.target.value })}
-            />
-            
-         <h5><label>Email:</label></h5>
-          <input
-            type="text" 
-            className="user"
-            placeholder="Enter email"
-         
-           onChange={(e) => this.setState({ email: e.target.value })}
-           />
+        <div className="wrapper">
+          <div className="title">Registration</div>
+          <form onSubmit={this.handleSubmit}>
+            <div className="field">
+              <input
+                type="text"
+                value={this.state.fname}
+                onChange={(e) => this.setState({ fname: e.target.value })}
+                required
+              />
+              <label>First Name</label>
+            </div>
+            <div className="field">
+              <input
+                type="text"
+                value={this.state.lname}
+                onChange={(e) => this.setState({ lname: e.target.value })}
+                required
+              />
+              <label>Last Name</label>
+            </div>
+            <div className="field">
+              <input
+                type="email"
+                value={this.state.email}
+                onChange={(e) => this.setState({ email: e.target.value })}
+                required
+              />
+              <label>Email Address</label>
+            </div>
+            <div className="field">
+              <input
+                type="password"
+                value={this.state.password}
+                onChange={(e) => this.setState({ password: e.target.value })}
+                required
+              />
+              <label>Password</label>
+            </div>
+            <div className="content">
+              <div className="checkbox">
+                <input type="checkbox" id="agree-terms" />
+                <label htmlFor="agree-terms">
+                  I agree to the terms and conditions
+                </label>
+              </div>
+            </div>
+            <div className="field">
+              <input type="submit" value="Register" />
+            </div>
            
-          <h5><label>Password:</label></h5>
-          <input
-            type="password"
-            className="user"
-            placeholder="Enter password" 
-            onChange={(e) => this.setState({ password: e.target.value })}
-            />
-           <br></br><br></br>
-        <div className="d-grid">
-        <button type="submit" onClick={this.validate} className='b'>Sign up</button>
+          </form>
         </div>
-        
-        <p className="p">
-          Already registered <Link to="/login">Log in</Link></p>
-
-      </form>
-      </div>
       </body>
-    )
+    );
   }
 }
