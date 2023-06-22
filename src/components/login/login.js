3900d81ff2a223831eba7edc02b65888ba1a1508
Rovind R <112811209+Rovind16@users.noreply.github.com>
2023-06-22 20:36:24 +0530

    Update login.js

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,60 +1,81 @@
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import React, { Component, useState } from "react";
-import "./login.css"
+import "./login.css";
+
 function Login() {
-   const [email, setEmail] = useState("");
-   const [password, setPassword] = useState("");
-   function handleSubmit(e) {
-      e.preventDefault();
-  
-      console.log(email, password);
-      fetch("https://bookie-backend.onrender.com/login-user", {
-        method: "POST",
-        crossDomain: true,
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-          "Access-Control-Allow-Origin": "*",
-        },
-        body: JSON.stringify({
-          email,
-          password,
-        }),
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          console.log(data, "userRegister");
-          if (data.status == "ok") {
-            alert("login successful");
-            window.localStorage.setItem("token", data.data);
-            window.localStorage.setItem("emaill", email);
-            window.localStorage.setItem("loggedIn", true);
-  
-              window.location.href = "./home";
-          }else{
-            alert('Username or Password Incorrect')
-          }
-        });
-    }
-    return(
-         <>
-         {/* <div class="login"> */}
-            <div className="h">
-            <form onSubmit={handleSubmit}>
-         <h2>LOGIN</h2>
-         {/* </div> */}
-            <h5><label>USERNAME:</label></h5>
-            <input type="text" placeholder="enter email" class="user" onChange={(e) => setEmail(e.target.value)}/>
-            <br/>
-            <h5><label>PASSWORD:</label></h5>
-            <input type="password" placeholder="enter password" class="password" onChange={(e) => setPassword(e.target.value)}/>
-            <br/>
-            <br/>
-            <input type="submit" value="LOGIN" class="submit"/>
-            </form>
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(email, password);
+
+    fetch("https://bookie-backend.onrender.com/login-user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data, "userRegister");
+        if (data.status === "ok") {
+          alert("login successful");
+          window.localStorage.setItem("token", data.data);
+          window.localStorage.setItem("email", email);
+          window.localStorage.setItem("loggedIn", true);
+          window.location.href = "./home";
+        } else {
+          alert("Username or Password Incorrect");
+        }
+      });
+  }
+
+  return (
+    <div className="wrapper">
+      <div className="title">Login</div>
+      <form onSubmit={handleSubmit}>
+        <div className="field">
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <label>Email Address</label>
+        </div>
+        <div className="field">
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <label>Password</label>
+        </div>
+        <div className="content">
+          <div className="checkbox">
+            <input type="checkbox" id="remember-me" />
+            <label htmlFor="remember-me">Remember me</label>
           </div>
-         </>
-    );
-    
+          <div className="pass-link">
+            <Link to="#">Forgot password?</Link>
+          </div>
+        </div>
+        <div className="field">
+          <input type="submit" value="Login" />
+        </div>
+        <div className="signup-link">
+          Not a member? <Link to="/register">Signup now</Link>
+        </div>
+      </form>
+    </div>
+  );
 }
+
 export default Login;
