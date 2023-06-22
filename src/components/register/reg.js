a482164094939e159a01e8f8406bf4140f2682b2
Rovind R <112811209+Rovind16@users.noreply.github.com>
2023-06-22 21:48:16 +0530

    Update reg.js

--- src/components/register/reg.js
+++ src/components/register/reg.js
@@ -1,49 +1,52 @@
 import React, { Component } from 'react';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import './reg.css'; // Importing the CSS file
 
 export default class App extends Component {
-  
   constructor(props) {
-    super(props);
+    super(props); // Call the parent class constructor
+
     this.state = {
-      fname: "",
-      lname: "",
-      email: "",
-      password: ""
+      fname: '',
+      lname: '',
+      email: '',
+      password: ''
     };
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSubmit(e) {
+  handleSubmit = (e) => {
     e.preventDefault();
+
     const { fname, lname, email, password } = this.state;
-    var value = [];
-    value.push(fname);
-    value.push(lname);
-    value.push(email);
-    value.push(password);
+
+    var values = [fname, lname, email, password];
     var f = 0;
-    for (var i = 0; i < value.length; i++) {
-      if (value[i] === "") {
+
+    for (var i = 0; i < values.length; i++) {
+      if (values[i] === '') {
         f = 1;
-        alert("All fields should be filled");
+        alert('All fields should be filled');
         break;
       }
     }
+
     var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
     if (!email.match(validRegex)) {
       f = 1;
-      alert("Email is invalid");
+      alert('Email is invalid');
     }
+
     console.log(fname, lname, email, password);
+
     if (f === 0) {
-      fetch("https://bookie-backend.onrender.com/register", {
-        method: "POST",
+      fetch('https://bookie-backend.onrender.com/register', {
+        method: 'POST',
         crossDomain: true,
         headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-          "Access-Control-Allow-Origin": "*"
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+          'Access-Control-Allow-Origin': '*'
         },
         body: JSON.stringify({
           fname,
@@ -54,67 +57,69 @@ export default class App extends Component {
       })
         .then((res) => res.json())
         .then((data) => {
-          console.log(data, "userRegister");
+          console.log(data, 'userRegister');
         });
-      alert("Sign up successful");
-      window.location.href = "/login";
+
+      alert('Sign up successful');
+      window.location.href = '/login';
     }
-  }
+  };
 
   render() {
     return (
       <body>
-        <div className="wrapper">
-          <div className="title">Registration</div>
+        <div className="box">
           <form onSubmit={this.handleSubmit}>
-            <div className="field">
-              <input
-                type="text"
-                value={this.state.fname}
-                onChange={(e) => this.setState({ fname: e.target.value })}
-                required
-              />
-              <label>First Name</label>
+            <h3>Sign Up</h3>
+            <h5>
+              <label>First name:</label>
+            </h5>
+            <input
+              type="text"
+              className="user"
+              placeholder="First name"
+              onChange={(e) => this.setState({ fname: e.target.value })}
+            />
+            <h5>
+              <label>Last name:</label>
+            </h5>
+            <input
+              type="text"
+              className="user"
+              placeholder="Last name"
+              onChange={(e) => this.setState({ lname: e.target.value })}
+            />
+
+            <h5>
+              <label>Email:</label>
+            </h5>
+            <input
+              type="text"
+              className="user"
+              placeholder="Enter email"
+              onChange={(e) => this.setState({ email: e.target.value })}
+            />
+
+            <h5>
+              <label>Password:</label>
+            </h5>
+            <input
+              type="password"
+              className="user"
+              placeholder="Enter password"
+              onChange={(e) => this.setState({ password: e.target.value })}
+            />
+            <br></br>
+            <br></br>
+            <div className="d-grid">
+              <button type="submit" className="b">
+                Sign up
+              </button>
             </div>
-            <div className="field">
-              <input
-                type="text"
-                value={this.state.lname}
-                onChange={(e) => this.setState({ lname: e.target.value })}
-                required
-              />
-              <label>Last Name</label>
-            </div>
-            <div className="field">
-              <input
-                type="email"
-                value={this.state.email}
-                onChange={(e) => this.setState({ email: e.target.value })}
-                required
-              />
-              <label>Email Address</label>
-            </div>
-            <div className="field">
-              <input
-                type="password"
-                value={this.state.password}
-                onChange={(e) => this.setState({ password: e.target.value })}
-                required
-              />
-              <label>Password</label>
-            </div>
-            <div className="content">
-              <div className="checkbox">
-                <input type="checkbox" id="agree-terms" />
-                <label htmlFor="agree-terms">
-                  I agree to the terms and conditions
-                </label>
-              </div>
-            </div>
-            <div className="field">
-              <input type="submit" value="Register" />
-            </div>
-           
+
+            <p className="p">
+              Already registered <Link to="/login">Log in</Link>
+            </p>
           </form>
         </div>
       </body>
